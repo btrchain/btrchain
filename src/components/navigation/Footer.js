baa678ab0608ee39e59b7245fc2625b4e1960d20
Mukul Rai <47410631+raimukul@users.noreply.github.com>
2021-09-02 12:49:10 +0530

    minor fix

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { AiFillInstagram, AiFillTwitterSquare, AiFillGoogleCircle,AiFillLinkedin } from "react-icons/ai";
 import './Footer.css'
 export default function Footer() {
     return (
@@ -10,7 +11,7 @@ export default function Footer() {
                         <Col md="3" lg="3" xl="3" className="mx-auto mt-3">
                             <h6 className="text-uppercase mb-4 font-weight-bold">BTR Chain</h6>
                             <p className='font-weight-bolder'>
-                                BTRChain is rated as one of the top web agencies in India by various industry magazines and review sites. 
+                                BTRChain is rated as one of the top web agencies in India by various industry magazines and review sites.
                                 We have a right blend of award-winning designers, expert web developers and Google certified digital marketers which make us a unique one-stop solution for hundreds of our clients, spread across 80+ countries.
                             </p>
                         </Col>
@@ -72,7 +73,7 @@ export default function Footer() {
                         </Col>
                         <hr className="w-100 clearfix d-md-none" />
                         <Col md="4" lg="3" xl="3" className="mx-auto mt-3">
-                        <h6 className="text-uppercase mb-4 font-weight-bold">Capabilities</h6>
+                            <h6 className="text-uppercase mb-4 font-weight-bold">Capabilities</h6>
                             <p>
                                 <a
                                     className="ext2 h6"
@@ -110,59 +111,51 @@ export default function Footer() {
                         </Col>
                     </Row>
                     <hr />
-                    <Row className="d-flex align-items-center">
-                        <Col md="8" lg="8">
-                            <p >
-                                &copy; {new Date().getFullYear()} Copyright:{" "}
-                                <a
-                                    className="ext2 h6 text-center" rel="noopener noreferrer" href="https://www.btrchain.com">
-                                    btrchain.com
-                                </a>
-                            </p>
-                        </Col>
-                        <Col md="4" lg="4" className="ml-lg-0">
-                            <div className="text-center text-md-right">
-                                <ul className="list-unstyled list-inline">
-                                    <li className="list-inline-item">
-                                        <a
+                    <p className='text-center'>
+                        &copy; {new Date().getFullYear()} Copyright:{" "}
+                        <a
+                            className="ext2 h6 text-center" rel="noopener noreferrer" href="https://www.btrchain.com">
+                            btrchain.com
+                        </a>
+                    </p>
+                    <ul className="list-unstyled list-inline pb-5">
+                        <li className="list-inline-item">
+                            <a
 
-                                            className="btn-floating btn-sm rgba-white-slight mx-1 ext2 h6"
-                                            href="https://www.instagram.com/"
+                                className="btn-floating btn-sm rgba-white-slight mx-1 ext2 h6"
+                                href="https://www.instagram.com/"
 
-                                        >
-                                            <i className="fab fa-instagram" />
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item">
-                                        <a
+                            >
+                                <AiFillInstagram className='footer-icons' />
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <a
 
-                                            className="btn-floating btn-sm rgba-white-slight mx-1 ext2 h6"
-                                            href="https://twitter.com/"
+                                className="btn-floating btn-sm rgba-white-slight mx-1 ext2 h6"
+                                href="https://twitter.com/"
 
-                                        >
-                                            <i className="fab fa-twitter" />
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item">
-                                        <a
-                                            className="btn-floating btn-sm rgba-white-slight mx-1 ext2 h6"
-                                            href="/"
-                                        >
-                                            <i className="fab fa-google-plus" />
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item">
-                                        <a
-                                            className="btn-floating btn-sm rgba-white-slight mx-1 ext2 h6"
-                                            href="/"
-                                        >
-                                            <i className="fab fa-linkedin-in" />
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </Col>
-                    </Row>
+                            >
+                                <AiFillTwitterSquare className='footer-icons' />
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <a
+                                className="btn-floating btn-sm rgba-white-slight mx-1 ext2 h6"
+                                href="/"
+                            >
+                                <AiFillGoogleCircle className='footer-icons' />
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <a
+                                className="btn-floating btn-sm rgba-white-slight mx-1 ext2 h6"
+                                href="/"
+                            >
+                                <AiFillLinkedin className='footer-icons' />
+                            </a>
+                        </li>
+                    </ul>
                 </Container>
             </footer>
         </div>
