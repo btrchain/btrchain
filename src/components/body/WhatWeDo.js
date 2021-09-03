7784a06ca23d69878a3bb72e5ffc62033486b835
Navimaurya <mauryanavis@gmail.com>
2021-09-03 23:21:42 +0530

    Contact us, About su page added

--- src/components/body/WhatWeDo.js
+++ src/components/body/WhatWeDo.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Card from '../cards/card-component'
 import backendImg from './images/backend.svg'
 import frontendImg from './images/frontend.svg'
 import devOpsImg from './images/devops.svg'
@@ -13,14 +12,37 @@ export default function WhatWeDo() {
                 <Container>
                     <p className='h3 font-weight-bold text-white text-center pt-5'>What we do?</p>
                     <div className="row">
-                        <div className="col-md-4 ">
-                            <Card title="Frontend Devlopment" src={frontendImg} />
+                        <div className="col-md-6 col-lg-3">
+                            <div className="card what-we-do-card" style={{ "background-image": "url('images/redline.svg')" }}>
+                                <img className="card-img-top" src={frontendImg} alt="what we do" />
+                                <div className="card-body text-center">
+                                    <p className="h3 py-2">Frontend Development</p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="col-md-4">
-                            <Card title="Backend Devlopment" src={backendImg} />
+                        <div className="col-md-6 col-lg-3">
+                            <div className="card what-we-do-card" style={{ "background-image": "url('images/redline.svg')" }}>
+                                <img className="card-img-top" src={backendImg} alt="what we do" />
+                                <div className="card-body text-center">
+                                    <p className="h3 py-2">Backend Development</p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="col-md-4">
-                            <Card title="Mobile App Devlopment" src={devOpsImg} />
+                        <div className="col-md-6 col-lg-3">
+                            <div className="card what-we-do-card" style={{ "background-image": "url('images/redline.svg')" }}>
+                                <img className="card-img-top" src={devOpsImg} alt="what we do" />
+                                <div className="card-body text-center">
+                                    <p className="h3 py-2">Mobile App Devlopment</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-lg-3">
+                            <div className="card what-we-do-card" style={{ "background-image": "url('images/redline.svg')" }}>
+                                <img className="card-img-top" src={devOpsImg} alt="what we do" />
+                                <div className="card-body text-center">
+                                    <p className="h3 py-2">Mobile App Devlopment</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </Container>
