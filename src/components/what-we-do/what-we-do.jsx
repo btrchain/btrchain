fbcc63b64b3cd77be14deeaa3a8ab5b717ccbe84
navimaurya <ncmaurya99@gmail.com>
2021-09-03 15:39:58 +0530

    w

--- src/components/what-we-do/what-we-do.jsx
+++ src/components/what-we-do/what-we-do.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 
 import Card from '../cards/card-component'
+import backendImg from './backend.svg'
+import frontendImg from './frontend.svg'
+import devOpsImg from './devops.svg'
 import './what-we-do.style.css'
+
+
 
 const WhatWeDo = () => {
     return (
         <div className="card-container"  >
             <div className='container'>
-                <h1 classNam='font-weight-bold text-white '>What we do?</h1>
+                <h1 className='font-weight-bold text-white '>What we do?</h1>
                 <div className="row">
                     <div className="col-md-4 ">
-                        <Card title="Frontend Devlopment" />
+                        <Card title="Frontend Devlopment" src={frontendImg} />
                     </div>
                     <div className="col-md-4">
-                        <Card title="Backend Devlopment" />
+                        <Card title="Backend Devlopment" src={backendImg} />
                     </div>
                     <div className="col-md-4">
-                        <Card title="Mobile App Devlopment" />
+                        <Card title="Mobile App Devlopment" src={devOpsImg} />
 
                     </div>
                 </div>
