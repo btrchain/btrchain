d7df35cbff28fc46e7fdacd044f01382ecf5250b
Mukul Rai <47410631+raimukul@users.noreply.github.com>
2021-09-04 22:17:32 +0530

    contact added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,18 +1,93 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Col, Container, Row, Form, Card, Button } from 'react-bootstrap'
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import './Contact.css'
+import { FaLocationArrow } from "react-icons/fa";
+import { MdEmail, MdPhone } from "react-icons/md";
+
+
 
 export default function Contact() {
     return (
         <div>
-            <div className='container-fluid'>
-                <Breadcrumb className='d-flex justify-content-center'>
-                    <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-                    <Breadcrumb.Item active>Contact us</Breadcrumb.Item>
-                </Breadcrumb>
-            </div>
-            <Container>
+            <div className='contactbg'>
+                <p className='h2 text-center pt-5'>Contact Us</p>
+                <div className='d-flex justify-content-center pe-auto'>
 
+                    <Breadcrumb >
+                        <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+                        <Breadcrumb.Item active>Contact us</Breadcrumb.Item>
+                    </Breadcrumb>
+                </div>
+            </div>
+            <Container className='pb-5 pt-3'>
+                <Row>
+                    <Col md='4'>
+                        <p className='h3'>Our Address</p>
+                        <p>01, University Road, <br></br>
+                            Kasna, Gautam Buddha University,<br></br>
+                            Greater Noida, UP 201312.</p>
+                        <br></br>
+                        <a href="/" className='text-decoration-none'> <p> <FaLocationArrow /> Location on google map</p></a>
+                        <p className="h5">Email us</p>
+                        <a href="mailto:contact@btrchain.com" className='text-decoration-none'><p><MdEmail /> contact@btrchain.com</p></a>
+                        <p className='h5'>Quick Call</p>
+                        <a href="tel:+91-9876543210" className='text-decoration-none'><p ><MdPhone /> +91-9876543210</p></a>
+                    </Col>
+                    <Col md='8'>
+                        <Card className='p-5 '>
+                            <Form>
+                                <Row>
+                                    <Col>
+                                        <Form.Group className="mb-3 h6" controlId="formBasicName">
+                                            <Form.Label>
+                                                Full Name
+                                            </Form.Label>
+                                            <Form.Control
+                                                className="mb-2"
+                                                id="inlineFormInput"
+                                                placeholder="Enter your name"
+                                            />
+                                        </Form.Group>
+                                    </Col>
+                                    <Col>
+                                        <Form.Group className="h6" controlId="formBasicEmail">
+                                            <Form.Label>Email</Form.Label>
+                                            <Form.Control type="email" placeholder="Enter email" />
+                                        </Form.Group>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <Form.Group className="mb-3 pt-2 h6" controlId="formBasicMobile">
+                                            <Form.Label>Contact Number</Form.Label>
+                                            <Form.Control type="number" placeholder="Enter mobile number" />
+                                        </Form.Group>
+                                    </Col>
+                                    <Col>
+                                        <Form.Label className="mb-3 h6">
+                                            Subject
+                                        </Form.Label>
+                                        <Form.Control
+                                            as="select"
+                                        >
+                                            <option>Select Subject</option>
+                                            <option>Development</option>
+                                            <option>Deployment</option>
+                                            <option>Business</option>
+                                            <option>Other</option>
+                                        </Form.Control>
+                                    </Col>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                        <Form.Label>Message</Form.Label>
+                                        <Form.Control as="textarea" rows={3} />
+                                    </Form.Group>
+                                </Row>
+                                <Button className='btn-demo'>Request</Button>
+                            </Form>
+                        </Card>
+                    </Col>
+                </Row>
             </Container>
         </div>
     )
