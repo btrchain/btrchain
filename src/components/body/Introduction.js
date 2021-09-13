0956aae9159a6f280e31c0aeef74b156d15090e8
krishnao01 <krishnaqw4141@gmail.com>
2021-09-13 21:51:54 +0530

    content update

--- src/components/body/Introduction.js
+++ src/components/body/Introduction.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import './Introduction.css'
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./Introduction.css";
 
 export default function Introduction() {
-    return (
-        <div className='intro-bg p-1'>
-            <Container>
-                <p className='h5 p-5 text-white'>BTRchain is consulting firm. We are grow exponentially. We works with the latest technologies and providing the solution for clients in the field of f.b.m.d. we helps start-up to convert their dream or ideas into reality and helps fully functional company to grow their business.
-                    We as a BTRians  are fully Passionate and highly motivated to thier work.</p>
-            </Container>
-        </div>
-    )
+  return (
+    <div className="intro-bg p-1">
+      <Container>
+        <p className="h5 p-5 text-white">
+          BTRchain is a consulting firm. We are growing exponentially. We works
+          with the latest technologies and providing the solution for clients in
+          the field of Frontend Backend Mobile App development and Devops. We
+          helps start-up to convert their dream or ideas into reality and helps
+          fully functional company to grow their business. We as a BTRians are
+          fully Passionate and highly motivated toward thier work.
+        </p>
+      </Container>
+    </div>
+  );
 }
