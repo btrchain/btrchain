be841537eccc2ef84559788f4a890d86875be181
btrchain <btrchain@gmail.com>
2022-03-21 00:23:35 +0530

    your message

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { createContext, lazy, Suspense } from "react";
+// import "./App.css";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/navigation/Header";
