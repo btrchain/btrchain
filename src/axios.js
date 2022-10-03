ba82b0c796f03f3e3b1a48d4e41111b911cb31c4
Mukul Rai <47410631+raimukul@users.noreply.github.com>
2022-10-03 19:49:09 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://client-btrchain.herokuapp.com',
+    baseURL: 'https://mesquite-pine-technosaurus.glitch.me',
   });
 
 
